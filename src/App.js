617222f191a5972f8aadff2b3fa5eab4f6505ab6
Roman354 <RomanBianko14@gmail.com>
2024-10-23 15:07:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect}  from 'react';
+
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const[modalWindowFlag, setModalWindowFlag] = useState(false);
+
+    console.log(modalWindowFlag);
+
+    function handlerClick(){
+        setModalWindowFlag(true)
+        console.log(modalWindowFlag)
+    }
+
+    return (
+        <div className="App">
+            <Clock />
+            <ModalWindow 
+                flag={modalWindowFlag}
+            />
+            <header className="App-header">
+                <div className="Page-container">
+                    <div className="Page-bookmark"><span className='Bookmarks-Name'>Google</span></div>
+                    <div className="Page-bookmark"><span className='Bookmarks-Name'></span></div>
+                    <div className="Page-bookmark"><span className='Bookmarks-Name'></span></div>
+                    <div className="Page-bookmark"><span className='Bookmarks-Name'></span></div>
+                    <div className="Page-bookmark"><span className='Bookmarks-Name'></span></div>
+                    <div className="Page-bookmark"><span className='Bookmarks-Name'>Ds</span></div>
+                    <div className="Page-bookmark"><span className='Bookmarks-Name'>yandex</span></div>
+                    <div 
+                        onClick={handlerClick}
+                    className="Page-bookmark"><span className='Bookmarks-Name'>Создать</span></div>
+                    
+                </div>
+           
+            </header>
+        </div>
   );
+}
+
+function ModalWindow(props){
+    // const [flagRender, setRenderFlag] = useState(props.flag);
+    
+
+
+    function handlerClick(){
+    
+        // setRenderFlag(false);
+        // console.log(flagRender);
+        // e.stopPropagation();
+    }
+
+
+    return(
+        <div className={ props.flag ? "ModalContainer" : "ModalContainer disable"}>
+                <span>Добавить Закладку</span>
+            <div className='ModalFlex'>
+                <span>Ссылка на страницу:</span>
+                <input className='InputModal' type="text" placeholder="https://google.com/"></input>
+            </div>
+            <div className='ModalFlex'>
+                <span>
+                    Название(не обязательно):
+                </span>
+                <input className='InputModal' type="text" placeholder="Google"></input>
+            </div>
+                <button onClick={handlerClick} className='ButtonModal'>Добавить</button>
+        </div>
+    )
+  
+}
+
+
+function Clock (){
+    const [time, setTime] = useState(new Date())
+    useEffect(()=>{
+        const interval = 
+        setInterval(() => {
+            setTime(new Date())          
+        }, 1000);
+        return(()=> clearInterval(interval))
+    }, [])
+
+    const hours = time.getHours() > 9? time.getHours(): "0"+ time.getHours();
+    const minutes = time.getMinutes() > 9 ? time.getMinutes(): "0"+time.getMinutes();
+    const seconds = time.getSeconds() > 9 ? time.getSeconds(): "0"+time.getSeconds();
+    const timeString = `${hours}:${minutes}:${seconds}`;
+
+    return(
+        <div className="ClockDiv">
+            <p>{timeString}</p>
+        </div>
+    )
+
 }
 
 export default App;
