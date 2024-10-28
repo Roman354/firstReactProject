bf7d1e9ac30a61890caf188978c410d6b7bf3f2e
Roman354 <RomanBianko14@gmail.com>
2024-10-28 18:21:23 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, {useState, useEffect, useRef}  from 'react';
 import useLocalStorage from "./useLocalStorage.js";
-
+import imgNotFound from '../src/imageNotFound.png';
 // const bookmarkArr = [
 //     {key:1, name:"Google", href:"https://www.google.ru/"},
 //     {key:2, name:"Youtube", href:"https://www.youtube.com/"},
@@ -9,27 +9,27 @@ import useLocalStorage from "./useLocalStorage.js";
 // ]
 
 function App() {
-    const [modalWindowFlag, setModalWindowFlag] = useState(false);
+    const [CreateModalWindowFlag, setCreateModalWindowFlag] = useState(false);
     const [bookmarkArr, setBookmarkArr] = useLocalStorage("bookmarks",  []);
     const [counterKey, setCounterKey] = useState(()=>{
         return bookmarkArr.length > 0 ?  Math.max(...bookmarkArr.map(item => item.key)) + 1 : 0;
     });
 
     function handleClick(){
-        setModalWindowFlag(true);
+        setCreateModalWindowFlag(true);
     }
 
     return (
         <div className="App">
             <Clock />
-            <ModalWindow 
+            <CreateModalWindow 
                 counterKey={counterKey}
                 setCounterKey={setCounterKey}
-                flag={modalWindowFlag}
+                flag={CreateModalWindowFlag}
                 bookmarks={bookmarkArr}
                 setBookmarkArr={setBookmarkArr}
                 cb={()=>{
-                    setModalWindowFlag(false)
+                    setCreateModalWindowFlag(false)
                 }}
             />
             <header className="App-header">
@@ -49,7 +49,6 @@ function App() {
 }
 
 function Bookmarks(props){
-
     function handleClick(key){
         props.setBookmarkArr(props.bookmarks.filter(a => a.key !== key))
         if(props.bookmarks.length === 1)
@@ -61,12 +60,14 @@ function Bookmarks(props){
     function getListBookmark () {
         return props.bookmarks.map(bookmark => 
             <a className="Link-bookmark" key={bookmark.key} target="_blank" rel="noreferrer" href={bookmark.href}>
-                <span 
-                    onClick={(e)=>{
-                        e.preventDefault()
-                        handleClick(bookmark.key)
-                    }}
-                    className="Delete-bookmark">X</span>
+                <div>
+                    <span 
+                        onClick={(e)=>{
+                            e.preventDefault()
+                            handleClick(bookmark.key)
+                        }}
+                        className="Delete-bookmark">✕</span>
+                </div>
                 <div className="Inscription-bookmark">
                     <img className="Img-bookmark" src={bookmark.img} alt={bookmark.name}></img>
                     <span className='Bookmarks-Name'>{bookmark.name}</span>
@@ -84,7 +85,6 @@ function Bookmarks(props){
 }
 
 function CreateBlock(props){
-
     return(
         <div 
             onClick={props.cb}
@@ -94,7 +94,7 @@ function CreateBlock(props){
     )
 }
 
-function ModalWindow(props){
+function CreateModalWindow(props){
     const [link, setLink] = useState("");
     const [nameLink, setNameLink] = useState("");
     const inputLinkRef = useRef(null);
@@ -144,7 +144,7 @@ function ModalWindow(props){
             getImg(name).then(imageUrl => {
                 getNewBookmarks(props.counterKey, name, link, imageUrl);
             }).catch(error => {
-                getNewBookmarks(props.counterKey, name, link, name);
+                getNewBookmarks(props.counterKey, name, link, imgNotFound);
                 console.error('Не удалось получить изображение:', error);
                
             });
@@ -159,7 +159,6 @@ function ModalWindow(props){
             ...props.bookmarks,
             {key:key, name: name, href: href, img: img}
         ]);
-
         inputNameRef.current.value = "";
         inputLinkRef.current.value = "";
         setLink("");
@@ -186,6 +185,77 @@ function ModalWindow(props){
                 }}
             >
                     <span>Добавить Закладку</span>
+                <div className='ModalFlex'>
+                    <span>Ссылка на страницу:</span>
+                    <input 
+                        ref={inputLinkRef}
+                        onChange={getInputValueLink}
+                        className='InputModal'
+                        type="text"
+                        placeholder="https://google.com/"></input>
+                </div>
+                <div className='ModalFlex'>
+                    <span>
+                        Название(не обязательно):
+                    </span>
+                    <input 
+                        ref={inputNameRef}
+                        onChange={getInputValueNameLink}
+                        className='InputModal' type="text" placeholder="Google"></input>
+                </div>
+                    <button onClick={() => {
+                        handleClick();
+                        props.cb();
+                    }
+                    } className='ButtonModal'>Добавить</button>
+            </div>
+        </div>
+    )
+}
+
+function ChangeModalWindow(props){
+    const [link, setLink] = useState("");
+    const [nameLink, setNameLink] = useState("");
+    const inputLinkRef = useRef(null);
+    const inputNameRef = useRef(null);
+    
+    function handleClick(){
+        // if(link.length){
+            // let name = nameLink;
+            // if(!nameLink.length)
+            // {
+            //     let domain = getDomainFromUrl(link);
+            //     inputNameRef.current.value = domain === false? link : domain;
+            //     name = inputNameRef.current.value;
+            // }
+     
+            // getImg(name).then(imageUrl => {
+            //     // getNewBookmarks(props.counterKey, name, link, imageUrl);
+            // }).catch(error => {
+            //     // getNewBookmarks(props.counterKey, name, link, imgNotFound);
+            //     console.error('Не удалось получить изображение:', error);
+               
+            // });
+
+        // }
+    }
+
+    function getInputValueLink(e){
+        setLink(e.target.value)   
+    }
+    function getInputValueNameLink(e){
+        setNameLink(e.target.value);
+    }
+    return(
+        <div onClick={()=>{
+            props.cb();
+        }}
+            className={props.flag ? "ModalBackground" : "ModalBackground disable"}>
+            <div className="ModalContainer" 
+                onClick={(e) => {
+                    e.stopPropagation();
+                }}>
+                <span>Добавить Закладку</span>
                 <div className='ModalFlex'>
                     <span>Ссылка на страницу:</span>
                     <input 
